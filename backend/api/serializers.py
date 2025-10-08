993dbfbd3b745e3db924ec1f9395b5631c9d5378
anasmuhd01 <anasmuhd010@gmail.com>
2025-10-08 07:21:21 +0100

    urls set

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSeializer(serializers.ModelSerializer):
+class  UserSeializer(serializers.ModelSerializer):
     class Meta:
       model = User
       fields = ["id", "username", "password"]
